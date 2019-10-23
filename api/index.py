e332736f9888cb2be522bb461293d1553bc4700f
edavidaja <e.david.aja@gmail.com>
2019-10-23 12:10:12 -0400

    logging to confirm that loading the model takes forever

--- api/index.py
+++ api/index.py
@@ -1,17 +1,23 @@
 from flask import Flask, request, jsonify, abort
 import joblib
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("index.py")
 app = Flask(__name__)
-
+logger.debug("before model")
 model = joblib.load("model/model.joblib")
+logger.debug("after model")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return 'Index Page'
+    return "Index Page"
 
-@app.route("/predict", methods = ["POST"])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    if not request.json or not 'text' in request.json:
+    if not request.json or not "text" in request.json:
         abort(400)
     text = [request.json["text"]]
 
