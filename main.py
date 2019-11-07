ff123ddff03b2d2c04150212fa89a165adc63b1c
edavidaja <e.david.aja@gmail.com>
2019-11-07 10:22:54 -0500

    tried switching to highmem and no compression

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
-import joblib
+import pickle
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS, cross_origin
 
 PORT = os.getenv("PORT", 8080)
 
+
 def load_model(path):
-    model = joblib.load(path)
+    with open(path, "rb") as f:
+        model = pickle.load(f)
     return model
+
+
+model = load_model("model/model.pkl")
 
 app = Flask(__name__)
 
@@ -23,7 +28,6 @@ def predict():
     if not request.json or not "text" in request.json:
         abort(400)
     text = [request.json["text"]]
-    model = load_model("model/model.joblib")
     prediction = model.predict_proba(text)
     out = prediction.tolist()
 
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import json
-import joblib
+import pickle
 import nltk
 import pandas as pd
 import os
@@ -28,10 +28,11 @@ X = data["full_text"].values.tolist()
 y = data["party_code"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-grid_parameters = {"tfidf__stop_words": (None, demonyms), "tfidf__min_df": (0.1, 1)}
+grid_parameters = {"tfidf__min_df": (0.1, 1)}
 
 grid_search = GridSearchCV(reg, grid_parameters, cv=5, n_jobs=-1, verbose=1)
 
 grid_search.fit(X_test, y_test)
 
-joblib.dump(grid_search.best_estimator_, "model/model.joblib", compress=1)
+with open("model/model.pkl", "wb") as f:
+    pickle.dump(grid_search.best_estimator_, f)
